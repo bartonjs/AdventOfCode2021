f8755c6ef87f16bad994cc4f5a9af5b481198e2c
Jeremy Barton <bartonjs@gmail.com>
2021-12-01 07:53:36 -0800

    Add project files.

---
+++ AdventOfCode2021/Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2021
+{
+    internal static class Data
+    {
+        private static string[] s_lines;
+
+        internal static IEnumerable<string> Enumerate()
+        {
+            foreach (string line in s_lines)
+            {
+                yield return line;
+            }
+        }
+
+        internal static List<long> ToLongList(this string commaSeparated)
+        {
+            ReadOnlySpan<char> span = commaSeparated;
+            int comma = commaSeparated.IndexOf(',');
+            List<long> list = new List<long>();
+
+            while (comma >= 0)
+            {
+                list.Add(long.Parse(span.Slice(0, comma)));
+                span = span.Slice(comma + 1);
+                comma = span.IndexOf(',');
+            }
+
+            list.Add(long.Parse(span));
+            return list;
+        }
+    }
+}
